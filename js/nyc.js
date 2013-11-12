b1f23027bfaf8c9c4fadbf7a1465740f98277341
Jennifer Creighton <jcreighton08@gmail.com>
2013-11-12 09:20:37 -0500

    cleaned up nyc.js

--- js/nyc.js
+++ js/nyc.js
@@ -1,12 +1,18 @@
-(function($) {
+$(function() {
 
-	var img = null;
 	var $scrollPosition = 0;
 	var $navLi = $('nav li a');
 	var $sections = $('.section');
 	var offsets = []; //[0, 1800, 2900, 3950, 4980]
 		
-
+		
+	//add active-line span & set left position
+	$('nav ul').append('<span class="active-line"></span>');
+	var $magicLine = $('.active-line');
+	$magicLine.css('left', $('.is-active').parent().offset().left);
+	
+		
+	// Animate magic line to new position on hover & back to original position off hover
 	$('nav li').hover(
 		function() {
 			animateLine($(this));
@@ -23,11 +29,12 @@
 			});	
 		}
 	);
+	
 
-
+	// On hover, grab img data from attribute and fade in image
 	$('.pin').hover(
 		function() {
-			img = "#" + $(this).data('img');
+			var img = "#" + $(this).data('img');
 			$(img).fadeIn('slow');
 		},
 		function() {
@@ -35,21 +42,19 @@
 			$(img).fadeOut('slow');
 		}
 		);
+		
 
-
+		// For each section, grab its offset.top
 		$sections.each(function(i, section){
 
 			var $section = $(section);
-	        var offset = $section.offset();
+	        var $offset = $section.offset();
 
-			offsets.push(offset.top);
+			offsets.push($offset.top);
 		});
 		
-		//on document ready, add active-line span & set left position
-		$('nav ul').append('<span class="active-line"></span>');
-		var $magicLine = $('.active-line');
-		$magicLine.css('left', $('.is-active').parent().offset().left);
-
+		
+		// on scroll, grab window's scrolltop, use parallax to scroll, and animate navigation during scroll
 		$(window).scroll(function(e) {
 			$scrollPosition = $(window).scrollTop();
 			parallax();
@@ -61,7 +66,7 @@
 		$('#map, #points').css('top', (0 - ((4.92 * $scrollPosition) * 0.1)) + 'px');
 	}	
 
-
+	// check current position against array of section offsets to set navigation magic line
 	function animateNavigation() {
 		$.each(offsets, function(i, section) {
 
@@ -90,7 +95,7 @@
 	}
 	
 
-})(jQuery);
+});
 
 
 						
