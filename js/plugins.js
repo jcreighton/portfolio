d34171f0b98c02c55eda3cf8c893f7a1a7ed3d53
Jenn Creighton <jcreighton08@gmail.com>
2019-03-30 11:16:11 -0700

    Update plugins.js

--- js/plugins.js
+++ js/plugins.js
@@ -1,24 +1,8 @@
-// Avoid `console` errors in browsers that lack a console.
-(function() {
-    var method;
-    var noop = function () {};
-    var methods = [
-        'assert', 'clear', 'count', 'debug', 'dir', 'dirxml', 'error',
-        'exception', 'group', 'groupCollapsed', 'groupEnd', 'info', 'log',
-        'markTimeline', 'profile', 'profileEnd', 'table', 'time', 'timeEnd',
-        'timeStamp', 'trace', 'warn'
-    ];
-    var length = methods.length;
-    var console = (window.console = window.console || {});
-
-    while (length--) {
-        method = methods[length];
-
-        // Only stub undefined methods.
-        if (!console[method]) {
-            console[method] = noop;
-        }
-    }
-}());
-
-// Place any jQuery/helper plugins in here.
+const container = document.getElementById('container');
+const root = ReactDOM.unstable_createRoot(container);
+root.render(
+  <div>
+    <h1>ColorBox</h1>
+    <ColorBox />
+  </div>
+);
